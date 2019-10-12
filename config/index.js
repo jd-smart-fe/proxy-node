7b2e482bb18e15ec60abeb483d95a08fc52c203a
shilili1 <shilili1@jd.com>
2019-10-12 11:55:20 +0800

    feat: 默认添加一个index.js文件

---
+++ config/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion:
+ * @Author: shilili1
+ * @Date: 2019-07-17 15:52:57
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2019-10-11 20:24:06
+ */
+module.exports = {
+    cookie: "_login_token=pass; _login_pin=laurel_aa",
+    host: "http://116.196.89.229:8081"
+}
