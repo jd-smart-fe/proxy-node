25ff3d284f25b92641ed501e6407db1496cb7d00
张明朝 <zhangmingchao9@jd.com>
2019-10-24 19:45:45 +0800

    feat:增加数据流格式多支持

--- routers/proxy.js
+++ routers/proxy.js
@@ -1,62 +1,74 @@
 const router = require('koa-router')();
-const axios = require ('axios');
+const axios = require('axios');
 const config = require('../config');
 const log4js = require('log4js');
 const url = require('url');
-
+const formidable = require("formidable");
 const logger = log4js.getLogger();
 axios.defaults.headers.Cookie = config.cookie;
 
-//post请求，参数需要后端支持@requestBody，前端挂在post body里，具体值在params上
-router.post('/proxy*', async(ctx,next)=>{
-    const tarHost =  ctx.params[0];
-    const search = url.parse(ctx.request.url).search;
-    let params = ctx.request.body;
-    console.log("参数为：", params);
+// post请求，参数需要后端支持@requestBody，前端挂在post body里，具体值在params上
+// router.post('/proxy*', async(ctx,next)=>{
+//     const tarHost =  ctx.params[0];
+//     const search = url.parse(ctx.request.url).search;
+//     let params = ctx.request.body;
+//     console.log("参数为：", params);
 
-    let data;
-    if(search!=null) {
-        console.log("请求地址为",`${config.host}${tarHost}${search}`);
-        data = await axios.post(`${config.host}${tarHost}${search}`,params);
-    }else {
-        console.log("请求地址为",`${config.host}${tarHost}`);
-        data = await axios.post(`${config.host}${tarHost}`,params);
-    }
-    console.log("获取的数据为：", data.data);
-    ctx.body = data.data;
-    //
-})
+//     let data;
+//     if(search!=null) {
+//         console.log("请求地址为",`${config.host}${tarHost}${search}`);
+//         data = await axios.post(`${config.host}${tarHost}${search}`,params);
+//     }else {
+//         console.log("请求地址为",`${config.host}${tarHost}`);
+//         data = await axios.post(`${config.host}${tarHost}`,params);
+//     }
+//     console.log("获取的数据为：", data.data);
+//     ctx.body = data.data;
+//     //
+// })
 
 //get请求，参数是在url上，挂在了search变量上
-router.get('/proxy*', async(ctx,next)=>{
-    const tarHost =  ctx.params[0];
+router.get('/proxy*', async (ctx, next) => {
+    const tarHost = ctx.params[0];
     let params = ctx.request.body;
     const search = url.parse(ctx.request.url).search;
-    console.log("请求地址为",`${config.host}${tarHost}${search}`)
-    const data = await axios.get(`${config.host}${tarHost}${search}`,params);
+    console.log("请求地址为", `${config.host}${tarHost}${search}`)
+    const data = await axios.get(`${config.host}${tarHost}${search}`, params);
     console.log("获取的数据为：", data.data);
     console.log("Time:", new Date());
     ctx.body = data.data;
 })
 
 //delete请求，
-router.del('/proxy*', async(ctx,next)=>{
-  const tarHost =  ctx.params[0];
-  const search = url.parse(ctx.request.url).search;
-  let params = ctx.request.body;
-  console.log("参数为：", params);
+router.del('/proxy*', async (ctx, next) => {
+    const tarHost = ctx.params[0];
+    const search = url.parse(ctx.request.url).search;
+    let params = ctx.request.body;
+    console.log("参数为：", params);
 
-  let data;
-  if(search!=null) {
-      console.log("请求地址为",`${config.host}${tarHost}${search}`);
-      data = await axios.delete(`${config.host}${tarHost}${search}`,params);
-  }else {
-      console.log("请求地址为",`${config.host}${tarHost}`);
-      data = await axios.delete(`${config.host}${tarHost}`,params);
-  }
-  console.log("获取的数据为：", data.data);
-  ctx.body = data.data;
+    let data;
+    if (search != null) {
+        console.log("请求地址为", `${config.host}${tarHost}${search}`);
+        data = await axios.delete(`${config.host}${tarHost}${search}`, params);
+    } else {
+        console.log("请求地址为", `${config.host}${tarHost}`);
+        data = await axios.delete(`${config.host}${tarHost}`, params);
+    }
+    console.log("获取的数据为：", data.data);
+    ctx.body = data.data;
 })
 
+router.post('/proxy*', async (ctx, next) => {
+    var form = new formidable.IncomingForm();
+    form.parse(ctx.req, async function (err, fields, files) {
+        if (err) {
+            throw err;
+            return;
+        }
+        console.log(fields); //{ name: base64字符串 }
+    });
+    ctx.body = "end"
+
+})
 
 module.exports = router;
